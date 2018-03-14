428e1b107230ca4d8991723e1a3d6316c8b28558
theMugician <gslonina7@gmail.com>
2018-03-13 18:16:23 -0700

    Javascript layout config

---
+++ src/js/list.js
@@ -0,0 +1,28 @@
+const List = () => {
+  let list = [
+    {'type': 'Mansion','desc': 'This beautiful home in Point Grey is going for well over 2.4 Million. Start saving up for that down payment.','img': '1.jpg'},
+    {'type': 'Shack','desc': 'This is definitely a shack','img': '2.jpg'},
+    {'type': 'Shack','desc': 'This a Shack. There is now way Jim Pattison lives here','img': '3.jpg'},
+    {'type': 'Mansion','desc': 'This glamourous shed sold for $1,190,000','img': '6.jpg'},
+    {'type': 'Shack','desc': 'Just another shack. No mansion here','img': '4.jpg'},
+    {'type': 'Mansion','desc': 'Another beautiful point grey home that closed for over 9 million dollars. Most likely it will be a tear down.','img': '5.jpg'},
+    {'type': 'Mansion','desc': 'This 3 bedroom home is going for only $4,188,000. What a steal of a deal.','img': '7.jpg'},
+    {'type': 'Shack','desc': 'This shack has everything you need!','img': '8.jpg'},
+    {'type': 'Mansion','desc': 'Beautiful deal for over 1.3 million. BBQ included','img': '9.jpg'},
+    {'type': 'Mansion','desc': 'Get three bedrooms and a gate for 1.9 million','img': '10.jpg'},
+    {'type': 'Shack','desc': 'Perfect for those outdoor getaways','img': '11.jpg'},
+    {'type': 'Shack','desc': 'This is definitely no rain forest shack','img': '12.jpg'},
+    {'type': 'Mansion','desc': '1.39 million! This home might look a little rusty on the outside but with 5 bedrooms you can\'t beat the price','img': '13.jpg'},
+    {'type': 'Mansion','desc': 'This a steal for a lush green yard','img': '14.jpg'},
+    {'type': 'Shack','desc': 'This one\'s a little too obvious for a mansion','img': '15.jpg'},
+    {'type': 'Mansion','desc': 'Another east van special, too shacklike','img': '16.jpg'},
+    {'type': 'Mansion','desc': 'The keys await your new mansion','img': '17.jpg'},
+    {'type': 'Shack','desc': 'Great for your next weekend getaway in the woods','img': '18.jpg'},
+    {'type': 'Mansion','desc': 'This fine hastings east estate comes with a massive garden','img': '19.jpg'},
+    {'type': 'Shack','desc': 'Sometimes it can be confusing differentiating a shack from a mansion','img': '20.jpg'},
+    {'type': 'Mansion','desc': 'ENDLESS POTENTIAL to build your dream home or update the current one','img': '21.jpg'
+    }
+  ]
+  return list;
+}
+export default List
--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,61 @@
 /**
- * jTinder initialization
+ * @param {Object} config Stack configuration.
+ * @returns {Object} An instance of Stack object.
  */
-// $("#tinderslide").jTinder({
-// 	// dislike callback
-//     onDislike: function (item) {
-// 	    // set the status text
-//         $('#status').html('Dislike image ' + (item.index()+1));
-//     },
-// 	// like callback
-//     onLike: function (item) {
-// 	    // set the status text
-//         $('#status').html('Like image ' + (item.index()+1));
-//     },
-// 	animationRevertSpeed: 200,
-// 	animationSpeed: 400,
-// 	threshold: 1,
-// 	likeSelector: '.like',
-// 	dislikeSelector: '.dislike'
-// });
+const App = (config) => {
+  let app;
+  //let eventEmitter;
+  //let stack;
+  //let init;
+  let list;
+
+  const construct = () => {
+    app = {};
+    //eventEmitter = Sister();
+    list = [
+      {'type': 'Mansion','desc': 'This beautiful home in Point Grey is going for well over 2.4 Million. Start saving up for that down payment.','img': '1.jpg'},
+      {'type': 'Shack','desc': 'This is definitely a shack','img': '2.jpg'},
+      {'type': 'Shack','desc': 'This a Shack. There is now way Jim Pattison lives here','img': '3.jpg'},
+      {'type': 'Mansion','desc': 'This glamourous shed sold for $1,190,000','img': '6.jpg'},
+      {'type': 'Shack','desc': 'Just another shack. No mansion here','img': '4.jpg'},
+      {'type': 'Mansion','desc': 'Another beautiful point grey home that closed for over 9 million dollars. Most likely it will be a tear down.','img': '5.jpg'},
+      {'type': 'Mansion','desc': 'This 3 bedroom home is going for only $4,188,000. What a steal of a deal.','img': '7.jpg'},
+      {'type': 'Shack','desc': 'This shack has everything you need!','img': '8.jpg'},
+      {'type': 'Mansion','desc': 'Beautiful deal for over 1.3 million. BBQ included','img': '9.jpg'},
+      {'type': 'Mansion','desc': 'Get three bedrooms and a gate for 1.9 million','img': '10.jpg'},
+      {'type': 'Shack','desc': 'Perfect for those outdoor getaways','img': '11.jpg'},
+      {'type': 'Shack','desc': 'This is definitely no rain forest shack','img': '12.jpg'},
+      {'type': 'Mansion','desc': '1.39 million! This home might look a little rusty on the outside but with 5 bedrooms you can\'t beat the price','img': '13.jpg'},
+      {'type': 'Mansion','desc': 'This a steal for a lush green yard','img': '14.jpg'},
+      {'type': 'Shack','desc': 'This one\'s a little too obvious for a mansion','img': '15.jpg'},
+      {'type': 'Mansion','desc': 'Another east van special, too shacklike','img': '16.jpg'},
+      {'type': 'Mansion','desc': 'The keys await your new mansion','img': '17.jpg'},
+      {'type': 'Shack','desc': 'Great for your next weekend getaway in the woods','img': '18.jpg'},
+      {'type': 'Mansion','desc': 'This fine hastings east estate comes with a massive garden','img': '19.jpg'},
+      {'type': 'Shack','desc': 'Sometimes it can be confusing differentiating a shack from a mansion','img': '20.jpg'},
+      {'type': 'Mansion','desc': 'ENDLESS POTENTIAL to build your dream home or update the current one','img': '21.jpg'}
+    ]
+  };
+
+  construct();
+}
+  
+console.log(list);
+
+document.addEventListener('DOMContentLoaded', () => {
+  app.addEventListener('click', (e) => {
+    let item = e.target;
+  })
+})
+
+ /**
+ * @param {Object} config Stack configuration.
+ * @returns {Object} An instance of Stack object.
+ */
 
 
 
-// /**
-//  * Set button action to trigger jTinder like & dislike.
-//  */
-// $('.actions .like, .actions .dislike').click(function(e){
-// 	e.preventDefault();
-// 	$("#tinderslide").jTinder($(this).attr('class'));
-// });
-// $(document).ready(function(){
-//   $(".link").on('click touchstart',function(){
-//     console.log("clicked");
-//     window.location = "http://redbrickreative.com";
-//     //window.open("http://redbrickreative.com", '_blank');
-//   })
-
-//     var obj = {
-//       method: 'feed',
-//       display: 'popup',    
-//       link: 'https://shackormansion.com',
-//       source: 'https://shack-or-mansion.firebaseapp.com/img/share.jpg',
-//       //name: 'Shack or Mansion',
-//       //caption: 'Reference Documentation',
-//       //description: 'Using Dialogs to interact with people.'
-//     };
-
-//     function callback(response) {
-//       console.log(response);
-//     }
-
-//     //FB.ui(obj, callback);
-
-// });
+ /**
+ * @param {Object} config Stack configuration.
+ * @returns {Object} An instance of Stack object.
+ */
